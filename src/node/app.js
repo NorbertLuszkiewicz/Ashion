c19a26dd83a1ab591d6f2d8bf6e53df948bcb2f7
NorbertLuszkiewicz <norbertluszkiewicz00@gmail.com>
2020-09-22 13:07:50 +0200

    Fix bug

--- src/node/app.js
+++ src/node/app.js
@@ -7,16 +7,16 @@ const PORT = 9000;
 
 const app = express();
 
-app.get('/api/cards', (res, req) => {
-  res.setEncoding(cards);
+app.get('/api/cards', (req, res) => {
+  res.send(cards);
 });
 
-app.get('/api/best-products', (res, req) => {
-  res.setEncoding(bestProducts);
+app.get('/api/best-products', (req, res) => {
+  res.send(bestProducts);
 });
 
-app.get('/api/women-products', (res, req) => {
-  res.setEncoding(womenProducts);
+app.get('/api/women-products', (req, res) => {
+  res.send(womenProducts);
 });
 
 app.listen(PORT, () => console.log('server'));
