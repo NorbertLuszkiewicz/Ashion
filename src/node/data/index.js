eb86ae252529e0772f8bbfb2b21b4d25a9102c76
NorbertLuszkiewicz <norbertluszkiewicz00@gmail.com>
2020-09-22 09:18:07 +0200

    Change import photos

--- src/node/data/index.js
+++ src/node/data/index.js
@@ -1,51 +1,27 @@
-import photo2 from 'assets/categories/category-2.jpg';
-import photo3 from 'assets/categories/category-3.jpg';
-import photo4 from 'assets/categories/category-4.jpg';
-import photo5 from 'assets/categories/category-5.jpg';
-
-import productPhoto1 from 'assets/product/product-1.jpg';
-import productPhoto2 from 'assets/product/product-2.jpg';
-import productPhoto3 from 'assets/product/product-3.jpg';
-import productPhoto4 from 'assets/product/product-4.jpg';
-import productPhoto5 from 'assets/product/product-5.jpg';
-import productPhoto6 from 'assets/product/product-6.jpg';
-import productPhoto7 from 'assets/product/product-7.jpg';
-import productPhoto8 from 'assets/product/product-8.jpg';
-
-import women1 from 'assets/shop/shop-1.jpg';
-import women2 from 'assets/shop/shop-2.jpg';
-import women3 from 'assets/shop/shop-3.jpg';
-import women4 from 'assets/shop/shop-4.jpg';
-import women5 from 'assets/shop/shop-5.jpg';
-import women6 from 'assets/shop/shop-6.jpg';
-import women7 from 'assets/shop/shop-7.jpg';
-import women8 from 'assets/shop/shop-8.jpg';
-import women9 from 'assets/shop/shop-9.jpg';
-import women10 from 'assets/shop/shop-10.jpg';
-
 export const cards = [
   {
     title: 'Men’s fashion',
     quantity: '358 items',
     link: '/men',
+    photo: 'assets/categories/category-2.jpg',
   },
   {
     title: 'Kid’s fashion',
     quantity: '273 items',
     link: '/',
-    photo: photo3,
+    photo: 'assets/categories/category-3.jpg',
   },
   {
     title: 'Cosmetics',
     quantity: '159 items',
     link: '/',
-    photo: photo4,
+    photo: 'assets/categories/category-4.jpg',
   },
   {
     title: 'Accessories',
     quantity: '792 items',
     link: '/',
-    photo: photo5,
+    photo: 'assets/categories/category-5.jpg',
   },
 ];
 
@@ -53,46 +29,46 @@ export const bestProducts = [
   {
     title: 'Buttons tweed blazer',
     price: '$59.0',
-    photo: productPhoto1,
+    photo: 'assets/product/product-1.jpg',
     color: 'green',
     label: 'NEW',
   },
   {
     title: 'Flowy striped skirt',
     price: '$49.0',
-    photo: productPhoto2,
+    photo: 'assets/product/product-2.jpg',
   },
   {
     title: 'Lite slim dress',
     price: '$59.0',
-    photo: productPhoto3,
+    photo: 'assets/product/product-3.jpg',
   },
   {
     title: 'Slim striped pocket shirt',
     price: '$59.0',
-    photo: productPhoto4,
+    photo: 'assets/product/product-4.jpg',
   },
   {
     title: 'Fit micro corduroy shirt',
     price: '$59.0',
-    photo: productPhoto5,
+    photo: 'assets/product/product-5.jpg',
   },
   {
     title: 'Tropical Kimono',
     price: '$49.0',
-    photo: productPhoto6,
+    photo: 'assets/product/product-6.jpg',
     color: 'red',
     label: 'SALE',
   },
   {
     title: 'White bluse',
     price: '$59.0',
-    photo: productPhoto7,
+    photo: 'assets/product/product-7.jpg',
   },
   {
     title: 'Tropical shirt',
     price: '$49.0',
-    photo: productPhoto8,
+    photo: 'assets/product/product-8.jpg',
     color: 'red',
     label: 'SALE',
   },
@@ -102,7 +78,7 @@ export const womenProducts = [
   {
     title: 'Hooded jacket',
     price: '$99.0',
-    photo: women1,
+    photo: 'assets/shop/shop-1.jpg',
     color: 'green',
     label: 'NEW',
     category: 'jacket',
@@ -111,35 +87,35 @@ export const womenProducts = [
   {
     title: 'Basic bag',
     price: '$69.0',
-    photo: women2,
+    photo: 'assets/shop/shop-2.jpg',
     category: 'bag',
     cost: 69,
   },
   {
     title: 'Pleated dress',
     price: '$59.0',
-    photo: women3,
+    photo: 'assets/shop/shop-3.jpg',
     category: 'dress',
     cost: 59,
   },
   {
     title: 'Dark wash Xavi jeans',
     price: '$59.0',
-    photo: women4,
+    photo: 'assets/shop/shop-4.jpg',
     category: 'jeans',
     cost: 59,
   },
   {
     title: 'Ankle-cuff sandals',
     price: '$59.0',
-    photo: women5,
+    photo: 'assets/shop/shop-5.jpg',
     category: 'shoes',
     cost: 59,
   },
   {
     title: 'Rucksack',
     price: '$49.0',
-    photo: women6,
+    photo: 'assets/shop/shop-6.jpg',
     color: 'red',
     label: 'SALE',
     category: 'bag',
@@ -148,14 +124,14 @@ export const womenProducts = [
   {
     title: 'Pendant earrings',
     price: '$29.0',
-    photo: women7,
+    photo: 'assets/shop/shop-7.jpg',
     category: 'rings',
     cost: 29,
   },
   {
     title: 'Scarf',
     price: '$49.0',
-    photo: women8,
+    photo: 'assets/shop/shop-8.jpg',
     color: 'red',
     label: 'SALE',
     category: 'scraf',
@@ -164,14 +140,14 @@ export const womenProducts = [
   {
     title: 'Long boots',
     price: '$69.0',
-    photo: women9,
+    photo: 'assets/shop/shop-9.jpg',
     category: 'shoes',
     cost: 69,
   },
   {
     title: 'Winter hat',
     price: '$29.0',
-    photo: women10,
+    photo: 'assets/shop/shop-10.jpg',
     color: 'red',
     label: 'SALE',
     category: 'hat',
