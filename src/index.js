e3e2b1ae898df112a028b0fc0e66119dfa0021bf
NorbertLuszkiewicz <norbertluszkiewicz00@gmail.com>
2020-09-23 13:47:58 +0200

    Add store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import Root from './views/Root';
+import store from 'store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <Provider store={store}>
+      <Root />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
