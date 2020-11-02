b0b785ae9ad38111a4c76fb1b5e21ec27f8e0bde
NorbertLuszkiewicz <norbertluszkiewicz00@gmail.com>
2020-11-02 08:55:08 +0100

    Add mongose

--- src/node/app.js
+++ src/node/app.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { cards } from './data';
 import { cart } from './data';
 import { bestProducts } from './data';
 import { womenProducts } from './data';
+import config from './config';
+import userRoute from './routes/userRoute';
 
 const PORT = 9000;
 
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
+
 const app = express();
+
+app.use('/api/users', userRoute);
 
 app.get('/api/cards', (req, res) => {
   res.send(cards);
